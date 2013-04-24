dc829cf83a0888841f5661f395d706c7f9450ed7
Ben Wiklund <ben@daisyowl.com>
2013-04-23 19:18:47 -0700

    built

---
+++ dist/iv.js
@@ -0,0 +1,83 @@
+(function() {
+  var Application, IV;
+
+  IV = (function() {
+    function IV() {
+      this.members = {};
+    }
+
+    IV.prototype.define = function(name, deps, func) {
+      return this.members[name] = {
+        name: name,
+        deps: deps,
+        providerFunc: func,
+        instance: null,
+        startedProviding: false
+      };
+    };
+
+    IV.prototype.instance = function() {
+      var clone, k, name, provider, v, _ref;
+
+      clone = {};
+      _ref = this.members;
+      for (name in _ref) {
+        provider = _ref[name];
+        clone[name] = {};
+        for (k in provider) {
+          v = provider[k];
+          clone[name][k] = v;
+        }
+      }
+      return new Application(clone);
+    };
+
+    return IV;
+
+  })();
+
+  Application = (function() {
+    function Application(members) {
+      this.members = members;
+    }
+
+    Application.prototype.resolve = function(name) {
+      var args, dep, member;
+
+      member = this.members[name];
+      if (member.startedProviding) {
+        throw new Error("circular dependency on " + name);
+      }
+      member.startedProviding = true;
+      if (member.instance == null) {
+        args = (function() {
+          var _i, _len, _ref, _results;
+
+          _ref = member.deps;
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            dep = _ref[_i];
+            _results.push(this.resolve(dep));
+          }
+          return _results;
+        }).call(this);
+        member.instance = member.providerFunc.apply({}, args);
+      }
+      return member.instance;
+    };
+
+    return Application;
+
+  })();
+
+  if (typeof module !== "undefined" && module !== null) {
+    module.exports = function() {
+      return new IV;
+    };
+  }
+
+  if (typeof window !== "undefined" && window !== null) {
+    window.IV = IV;
+  }
+
+}).call(this);
